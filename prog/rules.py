3f4866e12842106d457bb123fc33534f276ed1d0
Devsstudent <devsstudent777@gmail.com>
2023-05-27 21:59:36 +0200

    rules4 part1 done

--- prog/rules.py
+++ prog/rules.py
@@ -6,6 +6,11 @@ def isVoyelle(letter):
 		return (True)
 	return (False)
 
+def isMajVoyelle(letter):
+	if (letter == 'A' or letter == 'E' or letter == 'I' or letter == 'O' or letter == 'U' or letter == 'Y'):
+		return (True)
+	return (False)
+
 def get_next_pos_voyelle(str, pos):
 	for elem in range(pos, len(str) - 1):
 		if (isVoyelle(str[elem])):
@@ -55,10 +60,39 @@ def rules3(str, original):
 	else:
 		return (rules2(rules1(switch_left(original))))
 
+def somme(word, idx):
+	res = 0
+	n = idx + 1
+	while (idx >= 0):
+		if (isVoyelle(word[idx])):
+			res = res + ord(word[idx]) * pow(2, n - idx)
+		idx = idx - 1
+	return (res)
+
+def getVp(vp):
+	while (vp >= 65):
+		if (isVoyelle(chr(vp))):
+			return (vp)
+		vp = vp - 1
+
+def get_val(vp, word, idx):
+	vp = getVp(vp)
+	return (((vp + somme(word, idx - 1)) % 95) + 32)
+
+def rules4(str):
+	li = list(str)
+	i = 0
+	while (i != len(li)):
+		if (not isVoyelle(li[i]) and not isMajVoyelle(li[i]) and li[i].isalpha()):
+			li.insert(i + 1, chr(get_val(ord(li[i]) - 1, li, i)))
+		i = i + 1;
+	return ("".join(li))
+
 str = "cosette"
 
 print(str)
 print(rules1(str))
 print(rules2(rules1(str)))
 print(rules3(rules2(rules1(str)), "cosette"))
+print(rules4("futur"));
 
